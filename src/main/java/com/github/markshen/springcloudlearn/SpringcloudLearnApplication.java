562f5a1a4bda5c7918153ae5334ac9db6714214d
e <18009838166@163.com>
2019-08-19 21:47:36 +0800

    springboot helloworld

---
+++ src/main/java/com/github/markshen/springcloudlearn/SpringcloudLearnApplication.java
@@ -0,0 +1,11 @@
+package com.github.markshen.springcloudlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudLearnApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudLearnApplication.class, args);
+	}
+}
