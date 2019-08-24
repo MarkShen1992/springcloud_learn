56c913f0f97b02861aac76323ba4c06ce8793872
e <18009838166@163.com>
2019-08-25 07:54:10 +0800

    modify the code

--- src/main/java/com/github/markshen/springcloudlearn/utils/MD5Utils.java
+++ src/main/java/com/github/markshen/springcloudlearn/utils/MD5Utils.java
@@ -1,10 +1,19 @@
 package com.github.markshen.springcloudlearn.utils;
 
+
+import org.apache.shiro.crypto.hash.Md5Hash;
+
 /**
  * MD5加密类
  */
 public class MD5Utils {
+
+    /**
+     * 加密密码
+     * @param password
+     * @return
+     */
     public String encryptPassword(String password) {
-        return null;
+        return Md5Hash.toString(password.getBytes());
     }
 }
