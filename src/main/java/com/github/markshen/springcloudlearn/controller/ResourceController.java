74dc6ce70f1713b68b98ff0ca101e034cae66660
e <18009838166@163.com>
2019-08-25 13:47:06 +0800

    add some Controller, user, resource, role

---
+++ src/main/java/com/github/markshen/springcloudlearn/controller/ResourceController.java
@@ -0,0 +1,10 @@
+package com.github.markshen.springcloudlearn.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.stereotype.Controller;
+
+@Api("资源操作接口")
+@Controller
+public class ResourceController {
+
+}
---
+++ src/main/java/com/github/markshen/springcloudlearn/controller/RoleController.java
@@ -0,0 +1,10 @@
+package com.github.markshen.springcloudlearn.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.stereotype.Controller;
+
+@Api("角色操作接口")
+@Controller
+public class RoleController {
+
+}
